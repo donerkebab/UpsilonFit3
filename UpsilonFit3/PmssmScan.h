2e72bc32aa66f1d312985d970028111b3a5919ce
Mike Saelim <mike.saelim@gmail.com>
2014-03-31 15:47:27 -0400

    put down some of the groundwork for UpsilonFit3::PmssmScan

--- UpsilonFit3/PmssmScan.h
+++ UpsilonFit3/PmssmScan.h
@@ -8,17 +8,50 @@
 #ifndef UPSILONFIT3_PMSSMSCAN_H
 #define	UPSILONFIT3_PMSSMSCAN_H
 
+#include <vector>
+
+#include <gsl/gsl_vector.h>
+
+#include "McmcScan.h"
+
 namespace UpsilonFit3 {
 
-    class PmssmScan {
+    class PmssmScan : public Mcmc::McmcScan {
     public:
-        PmssmScan();
+        PmssmScan(unsigned int num_chains,
+                unsigned int max_steps,
+                double burn_fraction,
+                gsl_vector const* benchmark_sm,
+                gsl_vector const* benchmark_msugra,
+                std::vector<unsigned int> parameter_key);
         virtual ~PmssmScan();
 
+/*        std::vector<std::pair<gsl_vector*, std::string> >
+        GenerateChainSeeds(unsigned int num_chains,
+                unsigned int max_tries_per_direction,
+                double initial_step_guess,
+                std::pair<double, double> likelihood_bounds);
+*/
     private:
         PmssmScan(PmssmScan const& orig);
         void operator=(PmssmScan const& orig);
 
+//        gsl_vector* ConvertMsugraToPmssm(gsl_vector const*);
+        
+//        bool IsValidParameters(gsl_vector const* parameters);
+        
+//        bool IsValidSpectrum();
+
+/*        void MeasurePoint(gsl_vector const* parameters,
+                gsl_vector*& measurements,
+                double& likelihood);
+*/
+        
+        gsl_vector* benchmark_sm_;
+        gsl_vector* benchmark_msugra_;
+        gsl_vector* benchmark_pmssm_;
+        
+        std::vector<unsigned int> parameter_key_;
     };
 
 }
